ac543096feca39949ec0b28474ff3da5106efd2d
Philippe forest <phforest@eyeontext.com>
2023-06-09 14:49:04 +0200

    +4

--- tasks.py
+++ tasks.py
@@ -44,4 +44,4 @@ def version(_):
     from pathlib import Path
 
     __version__ = get_version(Path(__file__).parent)
-    print(f"==!!! + 3 ====>>>>>>>>{__version__=}")
+    print(f"==!!! + 4 ====>>>>>>>>{__version__=}")
